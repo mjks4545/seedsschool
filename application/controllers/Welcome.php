220c7abc3ef75785ffaefe7ce10c0f0e0a533e5a
mjks4545 <mjks4545@gmail.com>
2016-05-12 19:10:26 +0500

    comment
    comment

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -6,7 +6,7 @@ class Welcome extends CI_Controller {
     public function index()
     {
         $this->load->view('include/header');
-        //$this->load->view('include/sidebar');
+        $this->load->view('include/sidebar');
         $this->load->view('welcome_message');
         $this->load->view('include/footer');
     }
