25b6d66610f8b331271ae48d5749fea94271c717
SE-Bilal <hazratbilal.riu@gmail.com>
2016-05-12 18:23:44 +0500

    seed school for configuration
    seed school for configuration

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+    
+    public function index()
+    {
+        $this->load->view('include/header');
+        //$this->load->view('include/sidebar');
+        $this->load->view('welcome_message');
+        $this->load->view('include/footer');
+    }
+}
